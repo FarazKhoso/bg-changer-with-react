32e59569ae0a43933f1fbe45e2c366024c2bc8d5
Faraz <161611764+FarazKhoso@users.noreply.github.com>
2024-10-16 05:40:34 -0700

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,22 @@
-import Image from "next/image";
-import
-export default function Home() {
-  const [color, setColor] = useState()
-   return (
-   <> 
-   </>
- 
+import React from 'react'
+import { useState } from 'react'
+
+const Page = () => {
+const [count, setcount] = useState(0)
+
+  let x = 1
+  let updateNumber = () => {
+    count + x
+  }
+  return (
+    <div>
+      <h1>
+        i have {x}
+      </h1>
+
+      <button onClick={updateNumber}> clik</button>
+    </div>
+  )
+}
+
+export default Page
